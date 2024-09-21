4c055e60bd484d937676201a169ac1216fee40a8
chucoyos <unodev@outlook.com>
2024-09-21 01:50:55 -0600

    Create users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "User was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :second_last_name, :username, :phone, :user_type, :contact_person)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "pages#index"
   devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
+  get "users/:id" => "users#show", as: :user
+  get "users" => "users#index", as: :users
+  get "users/:id/edit" => "users#edit", as: :edit_user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,7 +13,6 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
   # Defines the root path route ("/")
   # root "posts#index"
 end
