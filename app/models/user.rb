734422af3e0be7ed2b7c18dda848acbb58a1e292
chucoyos <unodev@outlook.com>
2024-09-23 22:39:01 -0600

    Add validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   belongs_to :role
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
