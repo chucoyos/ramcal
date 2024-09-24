01a4ecb83f3b641c52448066b54fe6bbea384001
chucoyos <unodev@outlook.com>
2024-09-23 22:27:37 -0600

    Add validation to Roles an Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   belongs_to :role
+  validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
