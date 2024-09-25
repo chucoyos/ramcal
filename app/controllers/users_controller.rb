9fec6320571a870ec87987b820765dca0432a0b6
chucoyos <unodev@outlook.com>
2024-09-24 23:53:06 -0600

    Destroy user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "User was successfully deleted."
+  end
+
   private
 
   def user_params
