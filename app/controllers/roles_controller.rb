ef3abb989dd74ca5594239d12ede90bc5dff5274
chucoyos <unodev@outlook.com>
2024-09-26 02:04:28 -0600

    Add role policy

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -4,31 +4,36 @@ class RolesController < ApplicationController
 
   # GET /roles or /roles.json
   def index
+    authorize current_user, :index?, policy_class: RolePolicy
     @roles = Role.all
   end
 
   # GET /roles/1 or /roles/1.json
   def show
+    authorize current_user, :show?, policy_class: RolePolicy
   end
 
   # GET /roles/new
   def new
+    authorize current_user, :create?, policy_class: RolePolicy
     @role = Role.new
     @permissions = Permission.all
   end
 
   # GET /roles/1/edit
   def edit
+    authorize current_user, :update?, policy_class: RolePolicy
     @permissions = Permission.all
   end
 
   # POST /roles or /roles.json
   def create
+    authorize current_user, :create?, policy_class: RolePolicy
     @role = Role.new(role_params)
 
     respond_to do |format|
       if @role.save
-        format.html { redirect_to @role, notice: "Role was successfully created." }
+        format.html { redirect_to @role, notice: "El rol ha sido creado." }
         format.json { render :show, status: :created, location: @role }
       else
         format.html { render :new, status: :unprocessable_entity }
