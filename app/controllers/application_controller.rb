267e44155af48ece5820f8b34149d27bd2043e11
chucoyos <unodev@outlook.com>
2024-09-26 01:31:46 -0600

    Add UserPolicy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+  def user_not_authorized
+    flash[:alert] = "No tienes autorización para realizar esta acción."
+    redirect_to(request.referrer || root_path)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
+    authorize current_user, :index?, policy_class: UserPolicy
     @users = User.all
   end
 
   def new
+    authorize current_user, :create?, policy_class: UserPolicy
     @user = User.new
     @roles = Role.all
   end
 
   def create
+    authorize current_user, :create?, policy_class: UserPolicy
     @user = User.new(user_params)
     if @user.save
       redirect_to users_path, notice: "User was successfully created."
@@ -20,14 +23,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    authorize current_user, :show?, policy_class: UserPolicy
     @user = User.find(params[:id])
   end
 
   def edit
+    authorize current_user, :update?, policy_class: UserPolicy
     @user = User.find(params[:id])
   end
 
   def update
+    authorize current_user, :update?, policy_class: UserPolicy
     @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "User was successfully updated."
@@ -38,6 +44,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    authorize current_user, :destroy?, policy_class: UserPolicy
     @user = User.find(params[:id])
     @user.destroy
     redirect_to users_path, notice: "User was successfully deleted."
