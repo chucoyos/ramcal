1a20512598ae8c2286b0520816763aefa3662d98
chucoyos <unodev@outlook.com>
2024-09-27 21:11:18 -0600

    Pass show, edit and create user test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,4 +15,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get new_user_url
     assert_response :success
   end
+  test "should edit user" do
+    get edit_user_url(@user)
+    assert_response :success
+  end
+  test "should show user" do
+    get user_url(@user)
+    assert_response :success
+  end
+  test "should create user" do
+    assert_difference("User.count") do
+      post members_url, params: { user: { email: "uno@gmail.com", first_name: "Uno", last_name: "Dos", password: "password", role_id: @role.id, username: "uno" } }
+  end
+end
 end
