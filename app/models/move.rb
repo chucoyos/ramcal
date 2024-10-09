eba00d205da87f0f7fdfa0c1bf558238e6a59ceb
chucoyos <unodev@outlook.com>
2024-10-09 04:22:45 -0600

    Add Active Storage

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,5 @@
 class Move < ApplicationRecord
   belongs_to :container
   validates :container_id, presence: true
+  has_many_attached :images
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # Defines the root path route ("/")
   # root "posts#index"
-  match "*path", to: "application#render_404", via: :all
+  # match "*path", to: "application#render_404", via: :all
 end
