e2503e7192416c79ea3f604b0e06a248554db716
chucoyos <unodev@outlook.com>
2024-10-14 14:47:56 -0600

    Add validation to container number

--- app/models/container.rb
+++ app/models/container.rb
@@ -2,6 +2,15 @@ class Container < ApplicationRecord
   has_many :eirs, dependent: :destroy
   has_many :moves, dependent: :destroy
   belongs_to :user
+  before_save :upcase_number
+  validates :number, presence: true, format: { with: /\A[A-Z]{4}\d{7}\z/i,
+                                                message: "Deben ser 4 letras mayúsculas y 7 números sin espacios" }
+  validates :size, presence: true
   validates :size, inclusion: { in: %w[20 40],
                                 message: "%{value} no es un tamaño válido" }
+  private
+
+  def upcase_number
+    self.number = number.upcase
+  end
 end
