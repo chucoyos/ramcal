95863c820ce93b562da8846c9863e1f1121ac257
chucoyos <unodev@outlook.com>
2024-10-22 16:53:38 -0600

    Add assets/channels/notification_channel

---
+++ app/assets/channels/notification_channel.js
@@ -0,0 +1,17 @@
+import { createConsumer } from "@rails/actioncable";
+
+const consumer = createConsumer("/cable"); // Creates a new ActionCable consumer
+
+consumer.subscriptions.create("NotificationChannel", {
+  connected() {
+    console.log("Connected to NotificationChannel!");
+  },
+
+  disconnected() {
+    console.log("Disconnected from NotificationChannel.");
+  },
+
+  received(data) {
+    console.log("Received data: ", data);
+  }
+});
