eda3477b6c40c936b665d3234cfb83ccb728d307
chucoyos <unodev@outlook.com>
2024-10-22 18:17:31 -0600

    Edit rediscloud var

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,2 +1,8 @@
 // Import all the channels to be used by Action Cable
 // import "../../assets/channels/notification_channel.js";
+import { Application } from "@hotwired/stimulus"
+import "channels";
+import "channels/notification_channel";
+
+const application = Application.start()
+
