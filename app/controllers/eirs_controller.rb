13f3c57a7bcb9f7dd89a0e73f51dc84728a2f75a
chucoyos <unodev@outlook.com>
2024-10-31 15:22:07 -0600

    Fix paginate for clients

--- app/controllers/eirs_controller.rb
+++ app/controllers/eirs_controller.rb
@@ -6,7 +6,7 @@ class EirsController < ApplicationController
   def index
     authorize current_user, :index?, policy_class: EirPolicy
     if current_user.role.name == "cliente"
-      @eirs = Eir.joins(:container).where(containers: { user_id: current_user.id }).order(created_at: :desc).first(1)
+      @eirs = Eir.joins(:container).where(containers: { user_id: current_user.id }).order(created_at: :desc).page(5)
     else
       @eirs = Eir.order(created_at: :desc).page(params[:page]).per(5)
     end
