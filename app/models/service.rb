d4b4449b54055e2f80cda4356c6e9c62fcf524f4
chucoyos <unodev@outlook.com>
2024-12-10 16:02:03 -0600

    Add lavado to Service::NAMES

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,6 @@
 class Service < ApplicationRecord
-  NAMES = [ "Camión-Piso", "Piso-Camión", "Camión-Camión", "Reacomodo", "Almacenaje" ].freeze
+  # NAMES = [ "Camión-Piso", "Piso-Camión", "Camión-Camión", "Reacomodo", "Almacenaje" ].freeze
+  NAMES = %w[Camión-Piso Piso-Camión Camión-Camión Almacenaje Lavado Reacomodo].freeze
 
   belongs_to :container, optional: true
   has_many :pricings
