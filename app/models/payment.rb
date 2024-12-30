a1ad691f3af8b482060a1bbd830a32354e0f4cfc
chucoyos <unodev@outlook.com>
2024-12-29 20:23:08 -0600

    Update invoice after payment and validation

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,25 @@
 class Payment < ApplicationRecord
   belongs_to :invoice
   MEANS = %w[ Transferencia Cheque Tarjeta Efectivo ].freeze
+
+  validate :amount_cannot_be_greater_than_total
+  after_save :update_invoice, if: -> { invoice.payments.sum(:amount) >= invoice.total }
+  after_destroy_commit :update_invoice if -> { invoice.payments.sum(:amount) < invoice.total }
+
+
+  private
+
+  def update_invoice
+    if invoice.payments.sum(:amount) >= invoice.total
+      invoice.update(status: "Pagada")
+    else
+      invoice.update(status: "Pendiente")
+    end
+  end
+
+  def amount_cannot_be_greater_than_total
+    if amount.present? && invoice.present? && (amount + invoice.payments.sum(:amount)) > invoice.total
+      errors.add(:amount, "El pago no puede ser mayor al total de la factura")
+    end
+  end
 end
