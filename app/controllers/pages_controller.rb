e190e2e5df095e102882e8f44ec483fef5f4bfd5
chucoyos <unodev@outlook.com>
2024-12-30 23:17:39 -0600

    Rename settings resource to catalogs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def index
   end
 
-  def settings
+  def catalogs
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   resources :roles
 
   root "pages#index"
-  get "settings" => "pages#settings", as: :settings
+  get "catalogs" => "pages#catalogs", as: :catalogs
   devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
   get "users/new" => "users#new", as: :new_user
   get "users/:id" => "users#show", as: :user
