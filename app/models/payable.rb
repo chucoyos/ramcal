91f15a070b0b5a89889e79edd01e04bdf9a70a55
chucoyos <unodev@outlook.com>
2025-01-24 17:16:12 -0600

    Add payables resources

--- app/models/payable.rb
+++ app/models/payable.rb
@@ -2,6 +2,8 @@ class Payable < ApplicationRecord
   PAYMENT_TYPES = [ "Pago", "Pago sin soporte", "Gasto a comprobar", "Comprobación sin soporte",
                     "Comprobación de gastos", "Devolución", "Nómina" ].freeze
 
+  PAYMENT_MEANS = [ "Efectivo", "Transferencia", "Cheque", "Tarjeta" ].freeze
+
   belongs_to :supplier
   belongs_to :user
 end
