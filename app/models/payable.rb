a80f34529091280a4b9852d9291048325ace382e
chucoyos <unodev@outlook.com>
2025-01-25 01:27:34 -0600

    Add validation to payables

--- app/models/payable.rb
+++ app/models/payable.rb
@@ -6,4 +6,10 @@ class Payable < ApplicationRecord
 
   belongs_to :supplier
   belongs_to :user
+
+  # validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :payment_type, presence: true, inclusion: { in: PAYMENT_TYPES }
+  validates :payment_means, presence: true, inclusion: { in: PAYMENT_MEANS }
+  validates :payment_concept, presence: true
+  validates :payment_date, presence: true
 end
