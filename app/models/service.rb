4dcc8464280fa71d37154950ec01a26f2b43956c
chucoyos <unodev@outlook.com>
2025-02-11 09:22:34 -0600

    Comment Service model :check_invoiced

--- app/models/service.rb
+++ app/models/service.rb
@@ -9,8 +9,8 @@ class Service < ApplicationRecord
   has_many :users, through: :pricings
   validates :name, presence: true, inclusion: { in: NAMES }
   # add validation to prevent deletion or editing of a service that has been invoiced
-  before_destroy :check_invoiced
-  before_update :check_invoiced
+  # before_destroy :check_invoiced
+  # before_update :check_invoiced
 
   def check_invoiced
     if invoiced
